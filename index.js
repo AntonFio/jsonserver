cb1a36fd8cacf72fd6340be479555a7d12c6ccc1
AntonFio <anton_fiolkovski@mail.ru>
2023-02-22 14:49:46 +0300

    feat:add

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // server.js
-const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
-server.use(middlewares);
-server.use(router);
-server.listen(port, () => {
-  console.log("JSON Server is running", port);
-});
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+ 
+server.use(middlewares)
+server.use('', router)
+server.listen(process.env.PORT || 5000, () => {
+  console.log('JSON Server is running')
+})
